f9ee59859a0587ecf4f897495d9f8274566cb750
camsmith126 <slamenterprises23@gmail.com>
2026-03-15 08:26:19 -0600

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("website");
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    const d = new Date(dateObj);
+    return d.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      timeZone: "UTC",
+    });
+  });
+
+  eleventyConfig.addFilter("shortDate", (dateObj) => {
+    const d = new Date(dateObj);
+    return d.toISOString().split("T")[0];
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["md", "njk", "html"],
+  };
+};
